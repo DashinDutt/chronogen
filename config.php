30c667b7117e1ea70afe759edbe969502007cce0
Vashisth Bhushan <vashisthbhushan@gmail.com>
2019-05-26 18:22:54 +0530

    ALL files
    Co-Authored-By: Uttkarsh Dutt <dashindutt@users.noreply.github.com>

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+  $config = array (
+  'db_host' => 'localhost',
+  'db_name' => 'u483874186_bckp',
+  'db_user' => 'u483874186_bckp',
+  'db_pswd' => 'duttisagoodboy',
+  'ldap_host' => '',
+  'ldap_dn' => '',
+);
+?>
